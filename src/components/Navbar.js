f337987d097b1f4b7292ae6c868e90dd58d2f5cb
sekhar-fullstack-dev <sekhar.fullstack.dev@gmail.com>
2023-12-17 11:15:16 +0530

    portfolio link changed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -104,7 +104,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/cssahu3988/portfolio"
+                href="https://github.com/cssahu3988/resume"
                 target="_blank"
                 className="fork-btn-inner"
               >
